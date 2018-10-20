9e96ea0772b789f682be812f1e86998446cf069c
Leo Liang <leoliang@gmail.com>
2018-10-20 16:31:02 +0800

    Rename dactyl_orig.clj back to dactyl.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject dactyl-keyboard "0.1.0-SNAPSHOT"
   :description "A parametrized, split-hand, concave, columnar, erogonomic keyboard"
   :url "http://example.com/FIXME"
-  ; :main dactyl-keyboard.dactyl
-  :main dactyl-keyboard.dactyl-orig
+  :main dactyl-keyboard.dactyl
+  ; :main dactyl-keyboard.dactyl-split
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
